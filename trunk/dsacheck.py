284cefbdac98f9635c5423f1ca3507384329e0ec
lior.gradstein <lior.gradstein@8a4527a5-0c26-0410-b9d9-e9f1c5c4928e>
2007-01-02 13:28:13 +0000

    Initial import

---
+++ trunk/dsacheck.py
@@ -0,0 +1,34 @@
+# -*- coding: latin-1 -*-
+"""
+Copyright (C) 2005 Adelux & Saur <contact@adelux.fr>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+from deblib import deblib
+from dsalib import dsalib
+
+if __name__ == '__main__':
+    dsaBase = dsalib.DSABase()
+    dsaBase.update_base()
+
+    p = deblib.DebPackages()
+
+    for advisory,e in dsaBase.dsa_advisories.items():
+        for package_version in e['fixedIn']:
+            if p.is_installed('<'+package_version):
+                package = package_version.split('_',2)[0]
+                version = p.find_package(package)
+                print '%s is an old version (%s), %s is available.' % (package,version,package_version)
+
